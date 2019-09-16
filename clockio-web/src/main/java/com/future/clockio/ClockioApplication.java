523cd133ad2bb15ac6827329bf7aa149f6f996e6
Natasha Valentina <natasha.valentina1998@gmail.com>
2019-09-16 21:56:28 +0700

    add initial Application

---
+++ clockio-web/src/main/java/com/future/clockio/ClockioApplication.java
@@ -0,0 +1,13 @@
+package com.future.clockio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClockioApplication.class, args);
+    }
+
+}
