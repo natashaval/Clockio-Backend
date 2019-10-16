784a4d3b2c1819837a43281e6530445efbf8abc9
Natasha Valentina <natasha.valentina1998@gmail.com>
2019-10-16 22:34:10 +0700

    rename versioning in pom

--- clockio-web/src/main/java/com/future/clockio/controller/HelloController.java
+++ clockio-web/src/main/java/com/future/clockio/controller/HelloController.java
@@ -7,6 +7,9 @@ import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
   @GetMapping("/hello")
@@ -16,6 +19,13 @@ public class HelloController {
 
   @GetMapping("/about")
   public String printAbout() {return "This is an about page!"; }
+
+  @GetMapping("/test")
+  public Map<String,String> printTest() {
+    Map<String,String> test = new HashMap<>();
+    test.put("test", "Hasil dari Test");
+    return test;
+  }
 
   @GetMapping("/api/x")
   public String apix() {
